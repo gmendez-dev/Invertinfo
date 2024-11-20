53dcca944456617a4d1b023aa9606a826fa09c14
gmendez-dev <gerardoamendez4141@gmail.com>
2024-11-19 18:41:38 -0600

    Config secret_key

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ import os
 import tempfile
 
 app = Flask(__name__,template_folder='template')
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 prediccion_data = None
 
 if __name__ == '__main__':
-    app.secret_key = os.getenv("SECRET_KEY")
     app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
 
 # Certificado desde la variable
